f039d2c90f60509cc5210d6f966b9a54afac5e04
keramatiai <davoud.keramati@gmail.com>
2026-09-12 18:41:31 +0330

    variables

---
+++ basics/variables/main.py
@@ -0,0 +1,13 @@
+first_name = "Davoud"
+last_name = "Nuha"
+age  = 20
+salary = 100000000
+weight = 79.500
+height  = 180.25
+
+print (first_name)
+print (last_name)
+print (age)
+print (salary)
+print (weight)
+print (height)
