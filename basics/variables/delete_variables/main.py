24db828930c46157296a8457b249547e7e511f51
keramatiai <davoud.keramati@gmail.com>
2026-09-12 18:16:10 +0330

    delete variables

---
+++ basics/variables/delete_variables/main.py
@@ -0,0 +1,10 @@
+counter = 100          # Creates an integer variable
+miles   = 1000.0       # Creates a floating point variable
+name    = "Zara Ali"   # Creates a string variable
+
+del counter
+
+print(f"counter:{counter} , miles:{miles} , name:{name}")
+""""
+NameError: name 'counter' is not defined
+"""
