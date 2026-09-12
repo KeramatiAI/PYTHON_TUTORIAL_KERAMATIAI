ff780e294aeb18b3bd3a7e51e5e6ad6adb723d80
keramatiai <davoud.keramati@gmail.com>
2026-09-12 18:58:25 +0330

    variables--> local_variables

---
+++ basics/variables/scopes/local_variables/main.py
@@ -0,0 +1,4 @@
+def get_information(first_name,last_name,age,salary,weight,height):
+   information = f"information is: {first_name},{last_name},{age},{salary},{weight},{height}"
+   return information
+print(get_information("Davoud", "Keramati",20,50000000,80.250,182.50))
