6866d3201c194ea92289e7a35b5557c15dbcf96f
keramatiai <davoud.keramati@gmail.com>
2026-09-12 19:14:08 +0330

    variables--> global_variables

--- basics/variables/scopes/global_variables/main.py
+++ basics/variables/scopes/global_variables/main.py
@@ -1,11 +1,11 @@
 # Global Variables
 
-first_name = "Davoud"
-last_name = "Keramati"
-age  = 20
-salary = 100000000
-weight = 79.500
-height  = 180.25
+first_name = "Davoud" # Global Variable
+last_name = "Keramati" # Global Variable
+age  = 20 # Global Variable
+salary = 100000000 # Global Variable
+weight = 79.500 # Global Variable
+height  = 180.25 # Global Variable
 
 def get_information():
    information = f"information is: {first_name},{last_name},{age},{salary},{weight},{height}"
