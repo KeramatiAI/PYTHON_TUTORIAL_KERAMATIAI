0e26332e5c7b61d1ddc2a54be8b422ae71663df7
keramatiai <davoud.keramati@gmail.com>
2026-09-13 10:00:39 +0330

    data_types

---
+++ basics/data_types/main.py
@@ -0,0 +1,19 @@
+# * Numeric Data Types
+# int
+# flot
+# complex
+# * String Data Types
+# * Sequence Data Types
+# list
+# tuple
+# range
+# * Binary Data Types
+# bytes
+# bytearray
+# memoryview
+# * Dictionary Data Type
+# * Set Data Type
+# set
+# frozenset
+# * Boolean Data Type
+# * None Type
