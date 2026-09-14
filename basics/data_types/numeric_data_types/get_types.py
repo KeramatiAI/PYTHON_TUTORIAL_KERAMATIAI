65d790cd9c6235a3192fd31a334b6b701d472a92
keramatiai <davoud.keramati@gmail.com>
2026-09-14 10:05:32 +0330

    add data_types-> get types

---
+++ basics/data_types/numeric_data_types/get_types.py
@@ -0,0 +1,5 @@
+var1 = -786       # int data type
+var2 = 32.3  # float data type
+var3 = 9.322e-36j   # complex data type
+
+print(f"type var1:{type(var1)} , type var2:{type(var2)} , type var3:{type(var3)}")
