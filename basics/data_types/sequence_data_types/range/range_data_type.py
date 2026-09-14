2b455a5049ce09a8e194376ad94d88c981f1f1bb
keramatiai <davoud.keramati@gmail.com>
2026-09-14 10:18:34 +0330

    add data_types-> sequence data types -> range

---
+++ basics/data_types/sequence_data_types/range/range_data_type.py
@@ -0,0 +1,8 @@
+for i in range(5):
+  print(i)
+  print("#############################")
+  for i in range(2, 5):
+      print(i)
+print("#############################")
+for i in range(1, 5, 2):
+  print(i)
