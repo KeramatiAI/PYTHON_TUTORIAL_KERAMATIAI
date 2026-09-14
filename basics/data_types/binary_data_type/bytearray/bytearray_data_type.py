79b629d2c8647cb78fe68df9f7f7dc1c2b4802e5
keramatiai <davoud.keramati@gmail.com>
2026-09-14 10:27:04 +0330

    add data_types-> binary data types -> bytearray

---
+++ basics/data_types/binary_data_type/bytearray/bytearray_data_type.py
@@ -0,0 +1,11 @@
+# Creating a bytearray from an iterable of integers
+value = bytearray([72, 101, 108, 108, 111])
+
+print(value)
+print(type(value))
+print("#################################")
+# Creating a bytearray by encoding a string
+val = bytearray("Hello", 'utf-8')
+
+print(val)
+print(type(val))
