5ea67245634abcec53f8bf942e83ae4664218973
keramatiai <davoud.keramati@gmail.com>
2026-09-14 10:23:46 +0330

    add data_types-> binary data types -> byte

--- basics/data_types/binary_data_type/byte/byte_data_type.py
+++ basics/data_types/binary_data_type/byte/byte_data_type.py
@@ -3,3 +3,10 @@ b1 = bytes([65, 66, 67, 68, 69])
 
 print(b1)
 print(type(b1))
+print("##########################")
+# Using prefix 'b' to create bytes
+b2 = b'Hello'
+print(b2)
+
+print(b2)
+print(type(b2))
