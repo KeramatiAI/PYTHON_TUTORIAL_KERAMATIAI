90cc9f04def9932000168e4337e5f97223293d34
keramatiai <davoud.keramati@gmail.com>
2026-09-14 10:41:46 +0330

    add data_types-> set data type

---
+++ basics/data_types/set_data_type/set_data_type.py
@@ -0,0 +1,16 @@
+set1 = {123, 452, 5, 6}
+set2 = {'Java', 'Python', 'JavaScript'}
+set3 = {'One', 'Two', 'Three', 1,2,3, (1.0, 2.0, 3.0)}
+set4 = {2023, "Python", 3.11, 5+6j, 1.23E-4}
+
+print(set1)
+print(type(set1))
+
+print(set2)
+print(type(set2))
+
+print(set3)
+print(type(set3))
+
+print(set4)
+print(type(set4))
