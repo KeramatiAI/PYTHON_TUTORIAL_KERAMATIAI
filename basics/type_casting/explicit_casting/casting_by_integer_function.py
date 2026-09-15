943a4df53cc23b34e793aec3253fd47340517c1a
keramatiai <davoud.keramati@gmail.com>
2026-09-15 21:23:18 +0330

    add type_casting->explicit_casting->int()

--- basics/type_casting/explicit_casting/casting_by_integer_function.py
+++ basics/type_casting/explicit_casting/casting_by_integer_function.py
@@ -1,5 +1,5 @@
 """
-Python int() Function
+1-Python int() Function
 Python's built-in int() function converts an integer literal to an integer object, a float to integer, and
 a string to integer if the string itself has a valid integer literal representation.
 """
@@ -31,5 +31,28 @@ g = ("10"+"01")
 g = int("10"+"01")
 
 print(type(g))
+print("#######################################")
+"""
+2-String to Integer
+The int() function returns an integer from a string object, only if it contains a valid integer representation.
+"""
+h = int("100")
+print(type(h))
+
+i = ("10"+"01")
+i = int("10"+"01")
+
+type(i)
+print("#######################################")
+# j = int("10.5") #ValueError: invalid literal for int() with base 10: '10.5'
+# k = int("Hello World")#ValueError: invalid literal for int() with base 10: 'Hello World'
+print("#######################################")
+"""
+Binary String to Integer
+The string should be made up of 1 and 0 only, and the base should be 2.
+"""
+l = int("110011", 2)
+print(l)
+
 
 
