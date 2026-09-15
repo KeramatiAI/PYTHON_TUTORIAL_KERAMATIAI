8039eb925679b85ef0255b650186fe939e06b6a7
keramatiai <davoud.keramati@gmail.com>
2026-09-15 10:03:48 +0330

    add primitive types

---
+++ basics/data_types/primitive_and_non-primitive_data_types/primitive_types/primitive_types.py
@@ -0,0 +1,8 @@
+# The primitive data types are the fundamental data types that are used to create complex data types (sometimes called complex data structures).
+# There are mainly four primitive data types, which are −
+#
+# Integers
+# Floats
+# Booleans, and
+# Strings
+
