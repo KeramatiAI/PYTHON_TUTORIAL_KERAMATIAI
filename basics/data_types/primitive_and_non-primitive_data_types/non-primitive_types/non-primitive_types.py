96b004c2c78c473b32620d22b810780deadac1ad
keramatiai <davoud.keramati@gmail.com>
2026-09-15 10:04:31 +0330

    add non-primitive types

---
+++ basics/data_types/primitive_and_non-primitive_data_types/non-primitive_types/non-primitive_types.py
@@ -0,0 +1,6 @@
+# The non-primitive data types store values or collections of values. There are mainly four types of non-primitive types, which are −
+#
+# Lists
+# Tuples
+# Dictionaries, and
+# Sets
