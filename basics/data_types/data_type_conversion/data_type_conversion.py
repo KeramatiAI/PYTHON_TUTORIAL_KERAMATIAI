5402d901537cfb2f5d3698b10ba494b9e9b922c9
keramatiai <davoud.keramati@gmail.com>
2026-09-15 10:12:11 +0330

    add data_type_conversion

---
+++ basics/data_types/data_type_conversion/data_type_conversion.py
@@ -0,0 +1,27 @@
+print("Conversion to integer data type")
+a = int(1)     # a will be 1
+b = int(2.2)   # b will be 2
+c = int("3.3")   # c will be 3 # ValueError: invalid literal for int() with base 10: '3.3'
+c = int("1002")   # c will be 1002
+
+print (a)
+print (b)
+print (c)
+
+print("Conversion to floating point number")
+a = float(1)     # a will be 1.0
+b = float(2.2)   # b will be 2.2
+c = float("3.3") # c will be 3.3
+
+print (a)
+print (b)
+print (c)
+
+print("Conversion to string")
+a = str(1)     # a will be "1"
+b = str(2.2)   # b will be "2.2"
+c = str("3.3") # c will be "3.3"
+
+print (a)
+print (b)
+print (c)
