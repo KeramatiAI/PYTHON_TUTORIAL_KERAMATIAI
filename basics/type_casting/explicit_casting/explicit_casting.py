f8446da704416bc939d2f4e7ecc3623549b08b04
keramatiai <davoud.keramati@gmail.com>
2026-09-15 11:21:17 +0330

    add type_casting->explicit_castings

---
+++ basics/type_casting/explicit_casting/explicit_casting.py
@@ -0,0 +1,31 @@
+# int() Function
+a = int(10)
+b = a
+
+print(type(b))
+
+print("#######################################")
+c = int(10.5) #converts a float object to int
+d = int(2*3.14) #expression results float, is converted to int
+
+print(type(c))
+print(type(d))
+
+print("#######################################")
+
+e = int(True)
+
+print(type(e))
+
+print("#######################################")
+
+f = int("100")
+
+print(type(f))
+
+g = ("10"+"01")
+g = int("10"+"01")
+
+print(type(g))
+
+
