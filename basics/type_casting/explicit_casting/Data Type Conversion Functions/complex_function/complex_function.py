f708388e4852385fdf9e92554df11196629daa32
keramatiai <davoud.keramati@gmail.com>
2026-09-17 05:35:30 +0330

    add type_casting->explicit_casting->complex function

--- basics/type_casting/explicit_casting/Data Type Conversion Functions/complex_function/complex_function.py
+++ basics/type_casting/explicit_casting/Data Type Conversion Functions/complex_function/complex_function.py
@@ -14,8 +14,32 @@ imag (optional) − It represents the imaginary part of the complex number. If n
 Return Value
 This function returns a complex number based on the provided real and imaginary parts or a string representing a complex number.
 """
+
+# example 1
 real = 2
 imaginary = 3
 result = complex(real, imaginary)
+print('The complex value obtained is:', result)
+print(type(result))
+
+# example 2
+real = 4
+result = complex(real)
+print('The complex value obtained is:', result)
+print(type(result))
+
+# example 3
+imaginary = 7
+result = complex(imag=imaginary)
+print('The complex value obtained is:', result)
+print(type(result))
+
+# example 4
+result = complex()
+print('The complex value obtained is:',result)
+print(type(result))
+
+# example 5
+result = complex("2+4j")
 print('The complex value obtained is:',result)
 print(type(result))
