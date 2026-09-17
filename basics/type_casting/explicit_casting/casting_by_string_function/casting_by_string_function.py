e58e8f88f178cf2a44cc7645092430b4a278c368
keramatiai <davoud.keramati@gmail.com>
2026-09-17 05:14:11 +0330

    add type_casting->explicit_casting->str() funtion

--- basics/type_casting/explicit_casting/casting_by_string_function/casting_by_string_function.py
+++ basics/type_casting/explicit_casting/casting_by_string_function/casting_by_string_function.py
@@ -15,3 +15,12 @@ print(var_string_2)
 var_string_3 = str(2/5)
 print(type(var_string_3))
 print(var_string_3)
+
+# Floating points in scientific notations using E or e and with positive or negative power are converted to string with str() function.
+var_string_4 = str(10E4)
+print(type(var_string_4))
+print(var_string_4)
+
+var_string_5 = str(1.23e-4)
+print(type(var_string_5))
+print(var_string_5)
