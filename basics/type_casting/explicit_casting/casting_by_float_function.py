88c1b27597d8d3e5ad1b9c317739970e520c521c
keramatiai <davoud.keramati@gmail.com>
2026-09-17 04:54:53 +0330

    add type_casting->explicit_casting->int() funtion

---
+++ basics/type_casting/explicit_casting/casting_by_float_function.py
@@ -0,0 +1,26 @@
+var_float_1 = float(9.99)
+print(type(var_float_1))
+
+var_float_2 = float(100)
+print(type(var_float_2))
+
+var_float_3 = float("9.99")
+print(type(var_float_3))
+
+var_float_4 = float("1,234.50")
+print(type(var_float_4))
+
+var_float_5 = float("1.00E4")
+print(type(var_float_5))
+
+var_float_6 = float("1.00E-4")
+print(type(var_float_6))
+
+
+var_float_7 = float(1)  # a will be 1.0
+var_float_8 = float(2.2)  # b will be 2.2
+var_float_9 = float("3.3")  # c will be 3.3
+
+print(var_float_7)
+print(var_float_8)
+print(var_float_9)
