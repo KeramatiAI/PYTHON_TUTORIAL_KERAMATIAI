6ed2364214c7e2bbf2efebcc671e7d01d571db23
keramatiai <davoud.keramati@gmail.com>
2026-09-17 05:21:25 +0330

    add type_casting->explicit_casting->str() funtion

--- basics/type_casting/explicit_casting/casting_by_string_function/casting_by_string_function.py
+++ basics/type_casting/explicit_casting/casting_by_string_function/casting_by_string_function.py
@@ -8,11 +8,11 @@ print(type(var_string_1))
 print(var_string_1)
 
 # Float to String
-var_string_2 =str(11.10)
+var_string_2 = str(11.10)
 print(type(var_string_2))
 print(var_string_2)
 
-var_string_3 = str(2/5)
+var_string_3 = str(2 / 5)
 print(type(var_string_3))
 print(var_string_3)
 
@@ -32,14 +32,23 @@ var_string_6 = str('True')
 print(type(var_string_6))
 print(var_string_6)
 
-var_string_7 = str([1,2,3])
+var_string_7 = str([1, 2, 3])
 print(type(var_string_7))
 print(var_string_7)
 
-var_string_8 = str((1,2,3))
+var_string_8 = str((1, 2, 3))
 print(type(var_string_8))
 print(var_string_8)
 
-var_string_9 = str({1:100, 2:200, 3:300})
+var_string_9 = str({1: 100, 2: 200, 3: 300})
 print(type(var_string_9))
 print(var_string_9)
+
+# Following is an example to convert number, float and string into string data type:
+a = str(1)  # a will be "1"
+b = str(2.2)  # b will be "2.2"
+c = str("3.3")  # c will be "3.3"
+
+print(a)
+print(b)
+print(c)
