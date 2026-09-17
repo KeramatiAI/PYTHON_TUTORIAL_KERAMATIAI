325c7a04e092cec912067c6f290e34521ead7cef
keramatiai <davoud.keramati@gmail.com>
2026-09-17 05:08:13 +0330

    add type_casting->explicit_casting->str() funtion

--- basics/type_casting/explicit_casting/casting_by_string_function/casting_by_string_function.py
+++ basics/type_casting/explicit_casting/casting_by_string_function/casting_by_string_function.py
@@ -1,8 +1,18 @@
 """
 # CASTING BY STRING FUNCTION str() ****************************
 """
+from basics.type_casting.explicit_casting.casting_by_float_function.casting_by_float_function import var_float_2
 
 # Integer to string
 var_string_1 = str(10)
 print(type(var_string_1))
 print(var_string_1)
+
+# Float to String
+var_string_2 =str(11.10)
+print(type(var_string_2))
+print(var_string_2)
+
+var_string_3 = str(2/5)
+print(type(var_string_3))
+print(var_string_3)
