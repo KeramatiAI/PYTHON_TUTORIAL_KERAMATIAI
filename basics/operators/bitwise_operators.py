012f1b5748c2e14ef427e896c2b5367aa2260b7e
Davoud Keramati <davoud.keramati@gmail.com>
2026-09-19 10:22:42 +0330

    Create bitwise_operators.py

---
+++ basics/operators/bitwise_operators.py
@@ -0,0 +1,33 @@
+"""
+# Bitwise Operators
+Operator	Name	Example
+&	AND	a & b
+|	OR	a | b
+^	XOR	a ^ b
+~	NOT	~a
+<<	Zero fill left shift	a << 3
+>>	Signed right shift	a >> 3
+"""
+a = 20            
+b = 10            
+
+print ('a=',a,':',bin(a),'b=',b,':',bin(b))
+c = 0
+
+c = a & b;        
+print ("result of AND is ", c,':',bin(c))
+
+c = a | b;     
+print ("result of OR is ", c,':',bin(c))
+
+c = a ^ b;        
+print ("result of EXOR is ", c,':',bin(c))
+
+c = ~a;           
+print ("result of COMPLEMENT is ", c,':',bin(c))
+
+c = a << 2;       
+print ("result of LEFT SHIFT is ", c,':',bin(c))
+
+c = a >> 2;       
+print ("result of RIGHT SHIFT is ", c,':',bin(c))
