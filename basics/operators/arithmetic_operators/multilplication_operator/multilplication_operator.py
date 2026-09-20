54988a9895546070552d2701b0f57af8a7bf17ea
keramatiai <davoud.keramati@gmail.com>
2026-09-20 10:43:32 +0330

    update operators->arithmetic_operators->multiplication_operator

---
+++ basics/operators/arithmetic_operators/multilplication_operator/multilplication_operator.py
@@ -0,0 +1,20 @@
+# multiply two integers
+a=10
+b=20
+print ("Multiplication of two integers")
+print ("a =",a,"b =",b,"a*b =",a*b)
+# multiply integer and float numbers
+a=10
+b=20.5
+print ("Multiplication of integer and float")
+print ("a=",a,"b=",b,"a*b=",a*b)
+
+a=-5.55
+b=6.75E-3
+print ("Multiplication of float and float")
+print ("a =",a,"b =",b,"a*b =",a*b)
+# multiply complex numbers
+a=10+5j
+b=20.5
+print ("Multiplication of complex and float")
+print ("a =",a,"b =",b,"a*b =",a*b)
