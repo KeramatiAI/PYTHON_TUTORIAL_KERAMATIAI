6228d474d8bb15afd7cdd9ae523c7e03b6e6a4b4
keramatiai <davoud.keramati@gmail.com>
2026-09-20 18:42:16 +0330

    update operators->comparison_operators

---
+++ basics/operators/comparison_operators/string_operands/comparison_string_operands.py
@@ -0,0 +1,7 @@
+print ("comparison of strings")
+a='BAT'
+b='BALL'
+print ("a=",a, "b=",b,"a<b is",a<b)
+print ("a=",a, "b=",b,"a>b is",a>b)
+print ("a=",a, "b=",b,"a==b is",a==b)
+print ("a=",a, "b=",b,"a!=b is",a!=b)
