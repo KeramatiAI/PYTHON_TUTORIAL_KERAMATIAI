ccadc04f7245a5afc1d47b3e29006826da58638d
keramatiai <davoud.keramati@gmail.com>
2026-09-20 10:58:49 +0330

    update operators->arithmetic_operators->floor_division_operator

---
+++ basics/operators/arithmetic_operators/floor_division_operator/floor_division_operator.py
@@ -0,0 +1,13 @@
+# floor division operator
+a=9
+b=2
+print ("a=",a, "b=",b, "a//b=", a//b)
+a=9
+b=-2
+print ("a=",a, "b=",b, "a//b=", a//b)
+a=10
+b=1.5
+print ("a=",a, "b=",b, "a//b=", a//b)
+a=-10
+b=1.5
+print ("a=",a, "b=",b, "a//b=", a//b)
