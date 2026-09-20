b1fd8cbdc611d8aca9ec432e576c71755bce109b
keramatiai <davoud.keramati@gmail.com>
2026-09-20 10:37:48 +0330

    update operators->arithmetic_operators->addition_operator

--- basics/operators/arithmetic_operators/addition_operator/addition_operator.py
+++ basics/operators/arithmetic_operators/addition_operator/addition_operator.py
@@ -1,4 +1,15 @@
+# add two integer numbers
 a=10
 b=20
 print ("Addition of two integers")
 print ("a =",a,"b =",b,"addition =",a+b)
+# add integer and float numbers
+a=10
+b=20.5
+print ("Addition of integer and float")
+print ("a =",a,"b =",b,"addition =",a+b)
+# add two complex numbers
+a=10+5j
+b=20.5
+print ("Addition of complex and float")
+print ("a=",a,"b=",b,"addition=",a+b)
