0fbb5312a35a4038c8f8bfd4bb4b0223c2028c69
keramatiai <davoud.keramati@gmail.com>
2026-09-20 10:51:43 +0330

    update operators->arithmetic_operators->division_operator

---
+++ basics/operators/arithmetic_operators/division_operator/division_operator.py
@@ -0,0 +1,38 @@
+# divide two integer numbers
+a=10
+b=20
+print ("Division of two integers")
+print ("a=",a,"b=",b,"a/b=",a/b)
+print ("a=",a,"b=",b,"b/a=",b/a)
+# divide two float numbers
+a=10
+b=-20.5
+print ("Division of integer and float")
+print ("a=",a,"b=",b,"a/b=",a/b)
+a=-2.50
+b=1.25E2
+print ("Division of float and float")
+print ("a=",a,"b=",b,"a/b=",a/b)
+# divide complex numbers
+a=7.5+7.5j
+b=2.5
+print ("Division of complex and float")
+print ("a =",a,"b =",b,"a/b =",a/b)
+print ("a =",a,"b =",b,"b/a =",b/a)
+# devide zero with (integer & float & complex)
+
+# zero and integer
+a=0
+b=2
+print ("a=",a,"b=",b,"a/b=",a/b)
+# print ("a=",a,"b=",b,"b/a=",b/a) # ZeroDivisionError: float division by zero
+# zero and float
+a=0
+b=2.5
+print ("a=",a,"b=",b,"a/b=",a/b)
+# print ("a=",a,"b=",b,"b/a=",b/a) # ZeroDivisionError: float division by zero
+# zero and complex
+a=0
+b=7.5+7.5j
+print ("a=",a,"b=",b,"a/b=",a/b)
+# print ("a=",a,"b=",b,"b/a=",b/a) # ZeroDivisionError: complex division by zero
